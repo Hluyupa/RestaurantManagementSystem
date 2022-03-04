2c648c7c686f5a0c035a77c5f03f2867b39077d0
Hluyupa Home <hluyupa@gmail.com>
2022-03-05 01:26:27 +0500

    jQuery

--- RestarauntWebApplication/RestarauntWebApplication/wwwroot/js/tableInfo.js
+++ RestarauntWebApplication/RestarauntWebApplication/wwwroot/js/tableInfo.js
@@ -2,7 +2,12 @@
 
 	$('.table').click(
 		function () {
+
+			$('.table').css({ "opacity": "0.3" })
 			$('.userInfo').html($(this).attr('description-data'));
+			//$(this).attr('fill', '#C8D35B');
+			$(this).css({ "opacity": "0.8" });
+
 		}
 	)
 });
